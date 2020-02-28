45e83aacfb24b6b5b8f7ac7998709b298f2c821d
Dollyg <dollyg@thoughtworks.com>
2020-02-28 11:38:29 +0530

    wip

--- src/components/location-client/Locations.tsx
+++ src/components/location-client/Locations.tsx
@@ -4,16 +4,28 @@ import { LocationApi } from "./api";
 const locationApi = new LocationApi("localhost", 7654);
 
 export class Locations extends React.Component {
+  state = {
+    locations: new Array()
+  };
   async componentDidMount() {
     let locations = await locationApi.list();
-    this.onReceiveData(locations);
-  }
-
-  onReceiveData(locations: any) {
-    alert(locations);
+    this.setState({
+      locations
+    });
   }
 
   render() {
-    return <h1> hello </h1>;
+    return (
+      <div>
+        <h1> Locations </h1>
+        <div>
+          <ul>
+            {this.state.locations.map(loc => (
+              <li id={loc.prefix}>{loc.prefix}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
